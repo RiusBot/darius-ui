6ba4b716ab956a393be1088c8a19cea2bd8a83ae
rayray <ray85625@gmail.com>
2021-10-29 17:46:49 +0800

    pre-release, remove material-kit ad button

--- src/components/account/AccountProfile.js
+++ src/components/account/AccountProfile.js
@@ -7,7 +7,7 @@ import {
   CardActions,
   CardContent,
   Divider,
-  Typography
+  Typography,
 } from '@material-ui/core';
 
 const user = {
@@ -16,58 +16,47 @@ const user = {
   country: 'USA',
   jobTitle: 'Senior Developer',
   name: 'Katarina Smith',
-  timezone: 'GTM-7'
+  timezone: 'GTM+8'
 };
 
-const AccountProfile = (props) => (
-  <Card {...props}>
-    <CardContent>
-      <Box
-        sx={{
-          alignItems: 'center',
-          display: 'flex',
-          flexDirection: 'column'
-        }}
-      >
-        <Avatar
-          src={user.avatar}
+const AccountProfile = (props) => {
+  // const { user } = props;
+  return (
+    <Card {...props}>
+      <CardContent>
+        <Box
           sx={{
-            height: 100,
-            width: 100
+            alignItems: "center",
+            display: "flex",
+            flexDirection: "column",
           }}
-        />
-        <Typography
-          color="textPrimary"
-          gutterBottom
-          variant="h3"
         >
-          {user.name}
-        </Typography>
-        <Typography
-          color="textSecondary"
-          variant="body1"
-        >
-          {`${user.city} ${user.country}`}
-        </Typography>
-        <Typography
-          color="textSecondary"
-          variant="body1"
-        >
-          {`${moment().format('hh:mm A')} ${user.timezone}`}
-        </Typography>
-      </Box>
-    </CardContent>
-    <Divider />
-    <CardActions>
-      <Button
-        color="primary"
-        fullWidth
-        variant="text"
-      >
-        Upload picture
-      </Button>
-    </CardActions>
-  </Card>
-);
+          <Avatar
+            src={user.avatar}
+            sx={{
+              height: 100,
+              width: 100,
+            }}
+          />
+          <Typography color="textPrimary" gutterBottom variant="h3">
+            {user.name}
+          </Typography>
+          <Typography color="textSecondary" variant="body1">
+            {`${user.city} ${user.country}`}
+          </Typography>
+          <Typography color="textSecondary" variant="body1">
+            {`${moment().format("hh:mm A")} ${user.timezone}`}
+          </Typography>
+        </Box>
+      </CardContent>
+      <Divider />
+      <CardActions>
+        <Button color="primary" fullWidth variant="text">
+          Upload picture
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
 
 export default AccountProfile;
