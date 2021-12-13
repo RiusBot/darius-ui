92c2454b8e8702a09adc7231bb51626ee59bde06
rayray625 <ray85625@gmail.com>
2021-12-13 18:32:33 +0800

    update api endpoint

--- src/common/utils/getAxios.js
+++ src/common/utils/getAxios.js
@@ -3,9 +3,9 @@ import firebase from '@/utils/firebase';
 
 export default function getAxios() {
   const idToken = firebase.auth().currentUser.getIdToken(true)
-  
+
   const instance = defaultAxios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: process.env.NEXT_PUBLIC_DARIUS_BACKEND_API,
     headers: { Authorization: `Bearer ${idToken}` },
   });
   return instance;
