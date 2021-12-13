ad31972d95bb4beae4d164329af728a58e73ba5b
rayray625 <ray85625@gmail.com>
2021-12-14 02:32:27 +0800

    Fix idtoken by await promise

--- src/common/utils/getAxios.js
+++ src/common/utils/getAxios.js
@@ -1,8 +1,8 @@
 import defaultAxios from 'axios';
 import firebase from '@/utils/firebase';
 
-export default function getAxios() {
-  const idToken = firebase.auth().currentUser.getIdToken(true)
+export default async function getAxios() {
+  const idToken = await firebase.auth().currentUser.getIdToken(true)
 
   const instance = defaultAxios.create({
     baseURL: process.env.NEXT_PUBLIC_DARIUS_BACKEND_API,
