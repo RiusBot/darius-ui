279da17df77915faa2a1fdb3b3a17e0e85b63467
Irene Wang <ywangire@amazon.com>
2021-12-14 23:42:20 +0800

    Fix API settings form bugs

---
+++ src/features/api/api-create-form.js
@@ -0,0 +1,130 @@
+import { React, useState, useEffect } from 'react';
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import { Box, Typography, Button } from '@mui/material';
+import { CardContent, TextField } from '@mui/material';
+import { createUserApi } from '@/features/api/api-slice';
+
+const exchanges = ['Binance', 'FTX'];
+
+export const ApiCreateForm = (props) => {
+    const { display } = props;
+    const [isApiCreateReady, setApiCreate] = useState(false);
+    const [apiValues, setApiValues] = useState({key: '', secret: '', exchange: '', subaccount: ''});
+
+    const handleChange = (event) => {
+        setApiValues({
+        ...apiValues,
+        [event.target.name]: event.target.value
+        });
+    };
+
+    const checkValuesValid = (field) => {
+        switch (field) {
+            case 'key':
+            case 'secret':
+            return (apiValues[field].length != 0);
+            case 'exchange':
+            return (apiValues[field] == 'binance' || apiValues[field] == 'ftx');
+            case 'subaccount':
+            if (apiValues.exchange == 'ftx') {
+                return (apiValues[field].length != 0);
+            } else {
+                return true;
+            }
+        }
+    }
+
+    useEffect(() => {
+        function checkValuesReady() {
+        for (let i = 0; i < Object.keys(apiValues).length; i ++) {
+            if (!checkValuesValid(Object.keys(apiValues)[i])) {
+                return false;
+            }
+        }
+        return true;
+        }
+        const isApiReady = checkValuesReady();
+        setApiCreate(isApiReady)
+    }, [apiValues])
+
+
+    const createButtonClicked = () => {
+        const createApiInfo = {api: apiValues};
+        dispatch(createUserApi(createApiInfo));
+        setApiValues({key: '', secret: '', exchange: '', subaccount: ''});
+    }
+    if (!display) {
+        return (<></>);
+    }
+    return (
+    <>
+        <CardContent>
+            <Typography
+                color="textPrimary"
+                gutterBottom
+                variant="h6"
+                >
+                Please fill out the following form to add an API key.
+            </Typography>
+            <FormControl fullWidth>
+                <InputLabel >Exchange</InputLabel>
+                <Select
+                    name="exchange"
+                    id="exchange"
+                    value={apiValues.exchange}
+                    label="exchange"
+                    onChange={handleChange}
+                >
+                    {exchanges.map((exchange, idx) => (
+                        <MenuItem key={idx} value={exchange.toLowerCase()}>{exchange}</MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+            <TextField
+            sx={{display: (apiValues.exchange == 'ftx') ? 'flex' : 'none'}}
+            fullWidth
+            label="Subaccount"
+            margin="normal"
+            name="subaccount"
+            onChange={handleChange}
+            value={apiValues.subaccount}
+            variant="outlined"
+            />
+            <TextField
+            fullWidth
+            label="API key"
+            margin="normal"
+            name="key"
+            onChange={handleChange}
+            value={apiValues.key}
+            variant="outlined"
+            />
+            <TextField
+            fullWidth
+            label="Secret"
+            margin="normal"
+            name="secret"
+            onChange={handleChange}
+            value={apiValues.secret}
+            variant="outlined"
+            />
+        </CardContent>
+        <Box
+        sx={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+            p: 2
+        }}
+        >
+        <Button
+            color="primary"
+            variant="contained"
+            disabled={!isApiCreateReady}
+            onClick={createButtonClicked}
+        >
+            Add API Key
+        </Button>
+        </Box>
+    </>
+    );
+}
--- src/pages/apiSetting.js
+++ src/pages/apiSetting.js
@@ -1,76 +1,25 @@
 import { React, useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';;
 import Head from 'next/head';
-import { Box, Container, Typography, Button, IconButton } from '@mui/material';
-import { Card, CardHeader, CardContent, Divider, TextField } from '@mui/material';
-import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import { Box, Container, Typography, IconButton } from '@mui/material';
+import { Card, CardHeader, Divider, } from '@mui/material';
 import RemoveIcon from '@mui/icons-material/Remove';
 import { DashboardLayout } from '@/common/components/dashboard-layout';
 import { ConfirmDialog } from '@/features/dashboard/components/bot-management/confirm-dialog';
+import { ApiCreateForm } from '@/features/api/api-create-form';
 import { getUserApi } from '@/features/api/api-slice';
 import { getUserApiFromState } from '@/features/api/api-selector';
-import { createUserApi, deleteUserApi } from '@/features/api/api-slice';
-
-
-const exchanges = ['Binance', 'FTX'];
+import { deleteUserApi } from '@/features/api/api-slice';
 
 const apiSetting = () => {
   const dispatch = useDispatch();
-  const [isApiCreateReady, setApiCreate] = useState(false);
   const [apiDeleteDialog, setApiDeleteDialog] = useState({open: false, apiId: null});
-  const [apiValues, setApiValues] = useState({key: '', secret: '', exchange: '', subaccount: ''});
 
   useEffect (() => {
     dispatch(getUserApi());
     },[]
   );
   const userApi = useSelector(getUserApiFromState);
-
-  const handleChange = (event) => {
-    setApiValues({
-      ...apiValues,
-      [event.target.name]: event.target.value
-    });
-  };
-
-  const checkValuesValid = (field) => {
-    switch (field) {
-        case 'key':
-        case 'secret':
-          return (apiValues[field].length != 0);
-        case 'exchange':
-          return (apiValues[field] == 'binance' || apiValues[field] == 'ftx');
-        case 'subaccount':
-          if (apiValues.exchange == 'ftx') {
-            return (apiValues[field].length != 0);
-          } else {
-            return true;
-          }
-    }
-  }
-
-  useEffect(() => {
-    function checkValuesReady() {
-      for (let i = 0; i < Object.keys(apiValues).length; i ++) {
-        if (!checkValuesValid(Object.keys(apiValues)[i])) {
-            return false;
-        }
-      }
-      return true;
-    }
-    const isApiReady = checkValuesReady();
-    setApiCreate(isApiReady)
-  }, [apiValues])
-
-  const clearFormData = () => {
-    setApiValues({key: '', secret: '', exchange: '', subaccount: ''});
-  }
-
-  const createButtonClicked = () => {
-        const createApiInfo = {api: apiValues};
-        dispatch(createUserApi(createApiInfo));
-        clearFormData();
-    }
 
   const confirmDeleteApi = () => {
     console.log(apiDeleteDialog.apiId);
@@ -127,82 +76,6 @@ const apiSetting = () => {
     );
   }
 
-  const AddApiForm = () => {
-    if (Object.keys(userApi).length == 3) {
-      return (<></>);
-    }
-    return (<>
-        <CardContent>
-            <Typography
-                color="textPrimary"
-                gutterBottom
-                variant="h6"
-                >
-                Please fill out the following form to add an API key.
-            </Typography>
-            <FormControl fullWidth>
-                <InputLabel >Exchange</InputLabel>
-                <Select
-                    name="exchange"
-                    id="exchange"
-                    value={apiValues.exchange}
-                    label="exchange"
-                    onChange={handleChange}
-                >
-                    {exchanges.map((exchange, idx) => (
-                        <MenuItem key={idx} value={exchange.toLowerCase()}>{exchange}</MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
-            <TextField
-              sx={{display: (apiValues.exchange == 'ftx') ? 'flex' : 'none'}}
-              fullWidth
-              label="Subaccount"
-              margin="normal"
-              name="subaccount"
-              onChange={handleChange}
-              value={apiValues.subaccount}
-              variant="outlined"
-            />
-            <TextField
-              fullWidth
-              label="API key"
-              margin="normal"
-              name="key"
-              onChange={handleChange}
-              value={apiValues.key}
-              variant="outlined"
-            />
-            <TextField
-              fullWidth
-              label="Secret"
-              margin="normal"
-              name="secret"
-              onChange={handleChange}
-              value={apiValues.secret}
-              variant="outlined"
-            />
-        </CardContent>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'flex-end',
-            p: 2
-          }}
-        >
-          <Button
-            color="primary"
-            variant="contained"
-            disabled={!isApiCreateReady}
-            onClick={createButtonClicked}
-          >
-            Add API Key
-          </Button>
-        </Box>
-      </>
-    );
-  }
-
   return (
     <>
       <Head>
@@ -227,7 +100,9 @@ const apiSetting = () => {
             <CurrentApiList/>
           </Box>
           <Divider />
-          <AddApiForm/>
+          <ApiCreateForm 
+            display={Boolean(Object.keys(userApi).length < 3)}
+          />
         </Card>
         </Container>
       </Box>
