3b77d25cd610c8734bc2df6fbdd3085d34171843
Irene Wang <ywangire@amazon.com>
2021-12-14 18:36:29 +0800

    Add Backtest Time Chart for Daily Scalping Info Dialog

--- src/features/product/daily-info-dialog.js
+++ src/features/product/daily-info-dialog.js
@@ -6,7 +6,24 @@ import { TimeseriesChart } from '@/features/product/components/timeseries-chart'
 
 export const DailyScalpingInfoDialog = (props) => {
     const { open, onClose } = props;
+    const [completeRecords, setCompleteRecords] = React.useState([]);
 
+    React.useEffect(() => {
+        async function getData(file) {
+          const path = '/data/backtest_record/daily_scalping_backtest_' + file + '.csv';
+          const response = await fetch(path);
+          const reader = response.body.getReader();
+          const result = await reader.read(); // raw array
+          const decoder = new TextDecoder('utf-8');
+          const csv = decoder.decode(result.value); // the csv text
+          const results = Papa.parse(csv, { header: true }); // object with { data, errors, meta }
+          const data = results.data; // array of objects
+          return data;
+        }
+        getData('complete').then(data => {
+            setCompleteRecords(data);
+        });
+      }, []);
 
     return (
         <Dialog
@@ -39,7 +56,16 @@ export const DailyScalpingInfoDialog = (props) => {
                     'overflowY': 'scroll',
                     'padding': '16px 16px',
                 }}>
-                
+                <Box sx={{p:2}} >
+                    <Typography variant="h6" component="div" sx={{padding: '8px 0 16px'}}>
+                        完整歷史績效
+                    </Typography>
+
+                    <p style={{marginTop: 32}}>淨資產曲線：</p> 
+                    <TimeseriesChart
+                        data={completeRecords}
+                    />
+                </Box>
 
             </Box>
         </Dialog>
