2bb867e5c16aecb7bd8929c6f539a5d942cea54b
rayray625 <ray85625@gmail.com>
2022-01-13 20:17:32 +0800

    support email verification on own server

---
+++ src/pages/email_confirmation.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useDispatch } from 'react-redux'
+import Head from 'next/head';
+import { useFirebase } from 'react-redux-firebase'
+import { updateSnackbar } from '@/app/app-slice';
+import LinearProgress from '@mui/material/LinearProgress';
+
+const EmailConfirmation = () =>{
+  const router = useRouter()
+  const { mode, oobCode: actionCode } = router.query
+  const dispatch = useDispatch()
+  const firebase = useFirebase();
+  useEffect(() => {
+    firebase.auth().applyActionCode(actionCode)
+      .then((resp) => {
+        dispatch(updateSnackbar({ type: 'info', msg: 'Congratulations! You are a member now!'}));
+        router.push('/login')
+      })
+      .catch((error) => {
+        dispatch(updateSnackbar({ type: 'error', 
+          msg: 'Invalid or expired confirmation code, please verify email address again' }))
+        router.push('/register')
+      });
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Email Confirmation | RiusBot</title>
+      </Head>
+      <LinearProgress/>
+    </>
+  );
+}
+export default EmailConfirmation;
