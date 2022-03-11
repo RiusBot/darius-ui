dc562935a586a61ded75be234477ff2a80a7be2c
dada <bb04902103@gmail.com>
2022-03-11 06:52:39 +0000

    create_user_bot add hyperopt

--- src/features/dashboard/dashboard-saga.js
+++ src/features/dashboard/dashboard-saga.js
@@ -30,7 +30,8 @@ function* createUserBotSaga({ payload: createBotInfo }) {
       take_profit: createBotInfo.configOptions.takeProfit / 100,
       order_type: createBotInfo.configOptions.orderType,
       stop_loss_type: createBotInfo.configOptions.stopLossType,
-      take_profit_type: createBotInfo.configOptions.takeProfitType
+      take_profit_type: createBotInfo.configOptions.takeProfitType,
+      hyperopt: createBotInfo.configOptions.hyperopt
     },
     channel: createBotInfo.channel
   };
