880c3914f295afca07d903fc5b217d18acac36f0
Irene Wang <ywangire@amazon.com>
2022-06-06 00:30:38 +0800

    Add builtin pair detail dialog

--- src/features/pair/components/pair-detail-dialog.js
+++ src/features/pair/components/pair-detail-dialog.js
@@ -35,7 +35,11 @@ export const PairDetailDialog = (props) => {
                     </Typography>
                 </Box>
                 <IconButton
+<<<<<<< HEAD
                     style={{marginLeft: 'auto'}}
+=======
+                    style={{marginLeft: '64px'}}
+>>>>>>> 199cbb5 (Add builtin pair detail dialog)
                     onClick={onClose}
                 >
                     <CloseIcon fontSize="small" />
