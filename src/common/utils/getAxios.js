7623c8555d1b7bbbbd0b23295b8dac235dafeb6c
dada <bb04902103@gmail.com>
2022-06-19 16:02:01 +0000

    fix stupid

--- src/common/utils/getAxios.js
+++ src/common/utils/getAxios.js
@@ -5,8 +5,7 @@ export default async function getAxios() {
   const idToken = await firebase.auth().currentUser?.getIdToken();
 
   const instance = defaultAxios.create({
-    // baseURL: process.env.NEXT_PUBLIC_DARIUS_BACKEND_API,
-    baseURL: "http://35.240.214.114:8080",
+    baseURL: process.env.NEXT_PUBLIC_DARIUS_BACKEND_API,
     headers: { Authorization: `Bearer ${idToken}` },
   });
   return instance;
